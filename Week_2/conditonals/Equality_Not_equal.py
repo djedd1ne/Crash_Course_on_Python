a4cd2506f38ec9f5e4ba2b5ade555f6f553b40dd
Djallal Mekki <djmekki@2-C-1.42heilbronn.de>
2023-06-19 03:11:38 +0200

    conditons

---
+++ Week_2/conditonals/Equality_Not_equal.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+print(32 == 30 + 2) 	# The == operator checks if the 2 values 
+						# equals
+print(5 + 10 == 6 + 7)	# If the values are not equal, Python
+						#returns False
+print(10 - 4 != 10 + 4) # The != checks if the values are not equal
+						# if true, Python returns a True result
+print(9 / 3 != 3 * 1)	# Returns False value
