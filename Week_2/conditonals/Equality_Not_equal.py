218b1b1e7f302be7e11c79a37c57f6c986692e2a
Djallal Mekki <djmekki@2-C-1.42heilbronn.de>
2023-06-19 03:17:41 +0200

    assignment and comparaison

--- Week_2/conditonals/Equality_Not_equal.py
+++ Week_2/conditonals/Equality_Not_equal.py
@@ -7,3 +7,10 @@ print(5 + 10 == 6 + 7)	# If the values are not equal, Python
 print(10 - 4 != 10 + 4) # The != checks if the values are not equal
 						# if true, Python returns a True result
 print(9 / 3 != 3 * 1)	# Returns False value
+
+# The = assignment operator
+my_variable = 3 * 5
+print(my_variable)
+
+# The == equality comparaison operator
+print(my_variable == 3 * 5)
