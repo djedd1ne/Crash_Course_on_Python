7db9e2de5de229db9498c8cead34dbc39c398ab2
Djallal Mekki <djmekki@2-B-9.42heilbronn.de>
2023-06-30 05:46:11 +0200

    The equality vs the equals operator

--- Week_2/conditonals/Equality_Not_equal.py
+++ Week_2/conditonals/Equality_Not_equal.py
@@ -3,7 +3,7 @@
 print(32 == 30 + 2) 	# The == operator checks if the 2 values 
 						# equals
 print(5 + 10 == 6 + 7)	# If the values are not equal, Python
-						#returns False
+						# returns False
 print(10 - 4 != 10 + 4) # The != checks if the values are not equal
 						# if true, Python returns a True result
 print(9 / 3 != 3 * 1)	# Returns False value
@@ -13,4 +13,5 @@ my_variable = 3 * 5
 print(my_variable)
 
 # The == equality comparaison operator
-print(my_variable == 3 * 5)
+print(my_variable == 3 * 5) # Printing the variable returns a Boolean
+True						# True or False result.
